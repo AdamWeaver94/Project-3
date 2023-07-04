1523c95e33a7690ede6837955392d88522079cf6
Adam Weaver <adamweaver@live.co.uk>
2023-07-04 19:55:31 +0000

    add ship placement

--- run.py
+++ run.py
@@ -4,12 +4,12 @@
 import random
 
 # constants for game
-ship_sizes = [2,3,3,4,5]
-player_board = [[""] * 10 for i in range(10)]
-computer_board = [[""] * 10 for i in range(10)]
-player_guess_board = [[""] * 10 for i in range(10)]
-computer_guess_board = [[""] * 10 for i in range(10)]
-letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9}
+SHIP_SIZES = [2,3,3,4,5]
+PLAYER_BOARD = [[""] * 10 for i in range(10)]
+COMPUTER_BOARD = [[""] * 10 for i in range(10)]
+PLAYER_GUESS_BOARD = [[""] * 10 for i in range(10)]
+COMPUTER_GUESS_BOARD = [[""] * 10 for i in range(10)]
+LETTERS_TO-NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9}
 
 # board axis
 def print_board(board):
@@ -20,4 +20,21 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
-#
+# placing ships
+def place_ships(board):
+
+    for ship_length in SHIP_SIZES:
+
+        while True:
+            if board == COMPUTER_BOARD:
+                orientation, row, column = random.choice(["H", "V"]), random.randint(0,9), random.randint(0,9)
+                if check_ship_fit(ship_length, row, column, orientation):
+
+                    if ship_overlaps(board, row, column, orientation, ship_length) == False:
+
+                        if orientation == "H":
+                            for i in range(column, column + ship_length):
+                                board[row][i] = "X"
+                        else:
+                            for i in range(row, row, + ship_length):
+                                board[i][column] = "X"
